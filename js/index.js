c4248ff108c15405f687880610505e9db467360a
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2022-04-17 05:05:31 +1000

    show shortcuts in the console

--- js/index.js
+++ js/index.js
@@ -9,6 +9,39 @@ function userLightMode() {
     root.style.setProperty("--bg-color", "#f7f7f7");
 }
 
+
+(function printKeyboardShortcuts() {
+
+    function Shortcut(key, description) {
+        this.key = key;
+        this.description = description;
+    }
+
+    console.log("%cKeyboard shortcuts", "font-weight: bold; font-size: 1.5rem; text-decoration: underline; color: hotpink");
+    const shortcuts = `
+    Escape: Remove focus from all notepad components
+    Tab: Insert a tabspace (number of spaces can be altered from settings)
+    Ctrl + s: Save to local storage
+    Ctrl + Alt + c: Clear all notes
+    Shift + right arrow: Move to next note
+    Shift + left arrow: Move to previous note
+    Alt + s: Settings
+    `
+
+    // Table form
+    const shortcutList = [...shortcuts.trim().split("\n").map(line => new Shortcut(line.trim().split(":")[0], line.trim().split(":")[1]))]
+    console.table(shortcutList, ["key", "description"]);
+
+
+    // Normal test form
+    const shortcutsText = shortcuts.trim().split("\n").map(line => {
+        return "%c" + line.trim().split(":")[0] + ":" + "%c" + line.trim().split(":")[1]
+    }).join("\n");
+    const styles = Array(shortcuts.trim().split("\n").length).fill(["line-height: 1.5rem; color: hotpink", "color: white;"]).flat()
+    console.log(shortcutsText, ...styles)
+
+})();
+
 /**
  * [Keydown event: ESCAPE]
  * Pressing escape will remove focus from all notepad components
